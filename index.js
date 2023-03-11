b5d28a3ff6edd572f36cfab9bb816ec19bae2da0
Arpan <arpansinghrajput123@gmail.com>
2023-03-11 09:12:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Running....")
+var sub = document.getElementById("subscribe")
+document.querySelector('#subscribe').addEventListener("click", ()=>{
+sub.innerHTML = "Subscribed!!!"
+alert("You are succesfully Subscribe this!!!");
+})
